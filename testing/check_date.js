e1146b987408aa1f41d50c73d8a7e9511a1c7895
thesullivantage <38443606+thesullivantage@users.noreply.github.com>
2024-02-20 23:26:26 -0500

    remove testing

--- testing/check_date.js
+++
@@ -1,7 +0,0 @@
-const dateString = '2024-02-23T06:00:00-05:00';
-const date = new Date(dateString);
-const now = new Date();
-const options = { month: '2-digit', day: '2-digit' };
-const formattedDate = date.toLocaleDateString('en-US', options);
-
-console.log(formattedDate);
