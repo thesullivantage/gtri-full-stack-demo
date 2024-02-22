4ab4aaa36dd230a4264a7f9ec5838dc352bc9eec
thesullivantage <38443606+thesullivantage@users.noreply.github.com>
2024-02-21 21:10:03 -0500

    cleaned-up backend

--- back-end/Utils/BackEnd.js
+++ back-end/Utils/BackEnd.js
@@ -1,3 +1,18 @@
+// non-exported helper
+function parseTimezoneOffset(data_object, offsetSeconds) {
+    const date = new Date(data_object.time);
+    // UTC offset calculated in milliseconds 
+    const offsetMilliseconds = offsetSeconds * 1000;
+    // apply offset
+    const dateWithOffset = new Date(date.getTime() + offsetMilliseconds);
+    // re-create date string; can also use Date builtins
+    const date_str_offset = dateWithOffset.toLocaleDateString('en-US', { month: '2-digit', day: '2-digit' })
+    return {
+        time: dateWithOffset, 
+        day: date_str_offset
+    };
+};
+
 function DmsToDecimal(dms) {
     const parts = dms.split(/°|'|''| /).filter(Boolean);
     const degrees = parseFloat(parts[0]);
@@ -14,78 +29,81 @@ function DmsToDecimal(dms) {
     return decimal;
 };
 
-function ConvertDate(dateStr) {
-    /* converts date string returned by weather.gov forecast to */
-    const date = new Date(dateStr);
-    const options = { month: '2-digit', day: '2-digit' };
-    return date.toLocaleDateString('en-US', options);
-
-}
-
-function GetUnixTime() {
-    const now = new Date();
-    const oneWeekAgo = new Date(now.getTime() - (7 * 24 * 60 * 60 * 1000)); // Subtract 7 days in milliseconds
-
-    const nowUnix = Math.floor(now.getTime() / 1000); // Convert to seconds
-    const oneWeekAgoUnix = Math.floor(oneWeekAgo.getTime() / 1000); // Convert to seconds
-
-    return { present: nowUnix, weekAgo: oneWeekAgoUnix };
-};
-
-function ParseWeatherDat(weather_data) {
+function ParseWeatherDat(weather_data, station, timezone_info) {
     // Extract full timestamp, day, icon, temperature, and precipitation for the last hour
     const weather_data_formatted = weather_data.features.map(dat => {
         return {
+            // full timestamp from weather observation query
             time: dat.properties.timestamp,
+            // extract local date from full timestamp; can also use Date builtins
             day: new Date(dat.properties.timestamp).toLocaleDateString('en-US', { month: '2-digit', day: '2-digit' }),
-            icon: dat.properties.icon,
             temp: dat.properties.temperature.value,
             temp_unit: `${dat.properties.temperature.unitCode.split(':')[1].replace('deg', '°')}`,
-            // ternary: replace with 0 if null 
+            // ternary: replace with 0 if null: observed that 
             precip_last_hour: dat.properties.precipitationLastHour.value != null ? dat.properties.precipitationLastHour.value : 0,
             precip_unit: `${dat.properties.precipitationLastHour.unitCode.split(':')[1]}`
-        }
+        };
     })
-    console.log(weather_data_formatted)
 
-    // group data by day
+    // account for timezone difference 
+    const weather_data_formatted_timezone = weather_data_formatted.map(obj =>{
+        // calculate and apply timezone difference via helper (defined above)
+        const offset_dat = parseTimezoneOffset(obj, timezone_info.offset_seconds_UTC);
+        // re-assign existing time fields to adjusted ones
+        obj['time'] = offset_dat['time'];
+        obj['day'] = offset_dat['day'];
+        console.log('new obj: ', JSON.stringify(obj, null, '\t'));
+        return obj;
+    })
+
+    // group weather data by day
     const groupedDataDay = {};
-    weather_data_formatted.forEach(obj => {
+    weather_data_formatted_timezone.forEach(obj => {
         if (!groupedDataDay[obj.day]) {
             groupedDataDay[obj.day] = [];
-        }
+        };
         groupedDataDay[obj.day].push(obj);
     });
 
     // aggregate data by lows and highs 
-    let aggregated_day_data = {}
+    let aggregated_day_data = {};
+    // instantiate  initial global parameters (all days)
     aggregated_day_data['temp_unit'] = '';
-    aggregated_day_data['precip_unit'] = ''; 
+    aggregated_day_data['precip_unit'] = '';
+    aggregated_day_data['station'] = station;
+    
     Object.entries(groupedDataDay).forEach(([key, value]) => {
-        // set low and high limits easily reset for precipitation and temperature
-        let max_precip = 0;
-        let min_precip = 0;
-        let min_temp = 255;
-        let max_temp = -255;
-        let temp_unit = '';
-        let precip_unit = '';
+        // set each day's low and high limits for precipitation and temperature for a min/max pointer aggregation (per value) approach
+        let max_temp = Number.MIN_SAFE_INTEGER;
+        let min_temp = Number.MAX_SAFE_INTEGER;
+        let max_precip = Number.MIN_SAFE_INTEGER;
+        let min_precip = Number.MAX_SAFE_INTEGER;
         value.forEach(ind_dat => {
-            console.log(ind_dat)
             // set our temperature and precipitation units a single time in the global response object
             if (aggregated_day_data['precip_unit'] == '') {
-                aggregated_day_data['precip_unit'] = ind_dat.precip_unit
+                aggregated_day_data['precip_unit'] = ind_dat.precip_unit;
             }
             if (aggregated_day_data['temp_unit'] == '') {
-                aggregated_day_data['temp_unit'] = ind_dat.temp_unit
+                aggregated_day_data['temp_unit'] = ind_dat.temp_unit;
             }
-            const curr_temp = ind_dat.temp
-            const curr_precip = ind_dat.precip_last_hour
-            max_temp = max_temp > curr_temp ? max_temp : curr_temp;
-            min_temp = min_temp < curr_temp ? min_temp : curr_temp;
-            max_precip = max_precip > curr_precip ? max_precip : curr_precip;
-            min_precip = min_precip < curr_precip ? min_precip : curr_precip;
+            const curr_temp = ind_dat.temp;
+            const curr_precip = ind_dat.precip_last_hour;
+
+            // safeguard against null readings (I assume sensor being out of comission for these readings)
+            if (curr_temp !== null) {
+                // ternary logic: update min and max temperature for the day
+                max_temp = max_temp > curr_temp ? max_temp : curr_temp;
+                min_temp = min_temp < curr_temp ? min_temp : curr_temp;
+            }
+            // safeguard against null readings (including measured 0 for precipitation, it seems)
+            if (curr_precip !== null) {
+                // ternary logic: update min and max precipitation levels for the day
+                max_precip = max_precip > curr_precip ? max_precip : curr_precip;
+                min_precip = min_precip < curr_precip ? min_precip : curr_precip;
+            }
         })
         aggregated_day_data[key] = {
+            // store data; key is date
             max_precip: max_precip,
             min_precip: min_precip,
             min_temp: min_temp,
@@ -95,6 +113,4 @@ function ParseWeatherDat(weather_data) {
     return aggregated_day_data;
 }
 
-// Weather Parsing Controllers
-
-module.exports = { ParseWeatherDat, DmsToDecimal, GetUnixTime, ConvertDate };
+module.exports = { ParseWeatherDat, DmsToDecimal };
--- back-end/server.js
+++ back-end/server.js
@@ -1,45 +1,112 @@
 const express = require('express');
 const app = express();
-const {ParseWeatherDat, DmsToDecimal, ConvertDate, GetUnixTime } = require('./Utils/BackEnd');
-require('dotenv').config() // get environment variables
+const cors = require('cors');
+const { ParseWeatherDat, DmsToDecimal, ConvertDate, GetUnixTime } = require('./Utils/BackEnd');
+require('dotenv').config(); // get environment variables (in development)
+
+// Only use cross-origin resource sharing in development between two localhost domains (ports)
+app.use(cors());
 
 async function query_week(city, state, key) {
-    // uses forward geolocation to find latitude and longitude of City, ST
-    const geoloc_response = await fetch(`https://api.opencagedata.com/geocode/v1/json?q=${city},+${state}&key=${key}`);
 
+    // Query OpenCage to forward geolocate latitude and longitude from City, ST
+    // limit to US locations with country code
+    const geoloc_response = await fetch(`https://api.opencagedata.com/geocode/v1/json?q=${city},+${state}&key=${key}&countrycode=us`);
     const geoloc_data = await geoloc_response.json();
-    // format latitude, longitude for api.weather.gov call
+
+    // handle 404 not found for forward geolocation on OpenCage (no results returned)
+    if (geoloc_data.results.length === 0) {
+        throw {
+            status: 404,
+            message: `Error 404: geolocation not found for location`
+        };
+
+    // handle other errors within body of OpenCage response
+    } else if (geoloc_data.status.code !== 200) {
+        throw {
+            status: geoloc_data.status.code,
+            message: geoloc_data.status.message
+        };
+    };
+
+    // get local timezone info; pass to data processing function (ParseWeatherDat) below 
+    const timezone_info =  {
+        name: geoloc_data.results[0].annotations.timezone.name,
+        offset_seconds_UTC: geoloc_data.results[0].annotations.timezone.offset_sec, 
+    };
+
+    // Re-format latitude, longitude from Cartesian form to decimal for api.weather.gov call
     const latitude = DmsToDecimal(geoloc_data.results[0].annotations.DMS.lat);
     const longitude = DmsToDecimal(geoloc_data.results[0].annotations.DMS.lng);
-    // query api.weather.gov for metadata about City, ST location
+
+    // query api.weather.gov for metadata about latitude, longitude location; to first see what observation stations are returned
     const prelim_weather_response = await fetch(`https://api.weather.gov/points/${latitude},${longitude}`);
-    // convert location metadata response to JSON
     const prelim_weather_data = await prelim_weather_response.json();
 
-    // console.log(JSON.stringify(prelim_weather_data, null, '\t'))
-    console.log(prelim_weather_data.properties.observationStations)
-    const stations_response = await fetch(prelim_weather_data.properties.observationStations)
+    // handle non-network error responses
+    if (prelim_weather_data.status >= 400 || prelim_weather_data.status < 600) {
+        throw {
+            status: prelim_weather_data.status,
+            message: `Error ${prelim_weather_data.status} querying weather.gov metadata for latitude, longitude pair: ${prelim_weather_data.detail}`
+        };
+    };
+
+    // query stations for a location; observationStations endpoint returned from last call
+    const stations_response = await fetch(prelim_weather_data.properties.observationStations);
     const stations_data = await stations_response.json();
-    // select first observation station for location and get all observations (for the past week)
+
+    // handle non-network error responses
+    if (stations_data.status >= 400 || stations_data.status < 600) {
+        throw {
+            status: stations_data.status,
+            message: `Error ${stations_data.status} fetching weather stations for weather.gov grid location:\n${stations_data.statusText}`
+        };
+    }
+    // select first observation station for location and get all observations for the past week 
+    // 1 week: max length of data returned in real-time
     const weather_observation_response = await fetch(`${stations_data.observationStations[0]}/observations`)
-    console.log(stations_data.observationStations[0])
+    const weather_observation_data = await weather_observation_response.json();
 
-    // convert to JSON
-    const weather_data = await weather_observation_response.json();
-    // aggregate weather data
-    aggregated_day_data = ParseWeatherDat(weather_data)
+    // store (single) station we're reading from
+    const station = stations_data.observationStations[0].split('/').at(-1)
 
-    return aggregated_day_data}
+    // for this endpoint, if we have a status key, then endpoint error returned
+    // otherwise, response.ok/response.status not present
+    if (weather_observation_data.status !== undefined) {
+        // handle other errors with endpoint
+        if (weather_observation_data.status >= 400 || weather_observation_data.status < 600) {
+            throw {
+                status: weather_observation_data.status,
+                message: `Error ${weather_observation_data.status} for querying observations from weather.gov: ${weather_observation_data.detail}`
+            };
+        // empty result set doesn't return error response; handle here with 404
+        } else {
+            if (weather_observation_data.features.length === 0) {
+                throw {
+                    status: 404,
+                    message: `Error 404: Observations not found for ${stations_data.observationStations[0]}. Check that this is a valid station and correct.`
+                }
+            }
+        }
+    }
+    
+    // process and aggregate weather data
+    aggregated_day_data = ParseWeatherDat(weather_observation_data, station, timezone_info)
+    return aggregated_day_data
+}
 
+// single (GET) endpoint: get formatted weather data from a single weather station for City, ST in the US
 app.get('/weather/:city/:state', async function (req, res, next) {
-    console.log('hit')
-    // city and state extracted from params
+    // city and state extracted from route
     const city = req.params.city
     const state = req.params.state
     try {
+        // OpenCage API key stored in environment variable
         const weather_formatted = await query_week(city, state, process.env.OPENCAGE_GEOCODE_KEY)
         res.send(weather_formatted);
     } catch (err) {
+        // output error to console
+        console.log('error: ', JSON.stringify(err, null, '\t'))
         // Express 5.x < should pass error to next middleware automatically
         // but caught and sent to next middleware for more backwards compatability.  
         // i.e. if Heroku uses 4.x
@@ -48,8 +115,8 @@ app.get('/weather/:city/:state', async function (req, res, next) {
 });
 
 // generic error handling middleware function: accepts 4 arguments in express starting with err(or)
-app.use(function (err, req, res, next) {
-    // set result status to what is returned by error or 500 if cannot be resolved here
+app.use((err, req, res, next) => {
+    // set result status to what is returned by error or 500 if cannot be resolved here; more detailed handling in API querying middleware, above
     console.log(err)
     res.status(err.status || 500);
     // send error as response
@@ -62,5 +129,4 @@ app.listen(port, () => {
     console.log(`Weather demo server listening on port ${port}`)
 })
 
-// export express application if need to use in other scripts
-// module.exports = app;
+module.exports = app;
